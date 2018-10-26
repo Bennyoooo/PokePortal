ccd26c8ebb2bf81bf643ce9c74296a8fffdda72e
Xinhao Jiang <jiangxinhao@berkeley.edu>
2018-10-26 00:02:13 -0700

    master!

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :trainers
   root to: "home#index"
   resources :trainers
+  patch 'pokemons/capture', to: 'pokemon#capture', as: 'capture'
+  patch 'pokemons/damage', to: 'pokemon#damage', as: 'damage'
+  get 'pokemons/new', to: 'pokemon#new'
+  post 'pokemons/create', to: 'pokemon#create'
+  resources :pokemons
 end
---
+++ db/migrate/20181025222806_add_healthto_pokemons.rb
@@ -0,0 +1,4 @@
+class AddHealthtoPokemons < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
